01738ba63019225d9a20af08d5446af972a4962b
Karim El Qouns <karim.elqouns@7sensingsoftware.com>
2022-04-14 11:44:50 +0200

    commit 1

--- NN.py
+++ NN.py
@@ -1,6 +1,10 @@
 from modules import *
 from utilities import *
 
+# test branch 
+
+# test commit 1
+
 if __name__ == "__main__":
     X_train, y_train, X_test, y_test = load_data()
     X_train=X_train.reshape(X_train.shape[0],X_train.shape[1]*X_train.shape[2])/255
